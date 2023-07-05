270d40c7106d780ec02ca34e58adbeb475b05487
BobrovskaOlena <lena69114@gmail.com>
2023-07-06 00:15:18 +0300

    1

--- src/main/java/org/example/CrudServices/TicketCrudService.java
+++ src/main/java/org/example/CrudServices/TicketCrudService.java
@@ -1,2 +1,67 @@
-package org.example.CrudServices;public class TicketCrudService {
+package org.example.CrudServices;
+
+import org.example.entity.Ticket;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class TicketCrudService {
+    private final SessionFactory sessionFactory;
+
+    public TicketCrudService(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void createTicket(Ticket ticket) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Ticket mergedTicket = (Ticket) session.merge(ticket);
+        session.persist(mergedTicket);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public Ticket getTicket(Long id) {
+        Session session = sessionFactory.openSession();
+        Ticket ticket = session.get(Ticket.class, id);
+        System.out.println("Retrieved ticket: " + ticket.toString());
+        session.close();
+        return ticket;
+    }
+
+    public void getAllTickets() {
+        Session session = sessionFactory.openSession();
+        List<Ticket> tickets = session.createQuery("FROM Ticket", Ticket.class).list();
+        tickets.forEach(allTicket -> System.out.println("tickets list ==> " + allTicket.toString()));
+        session.close();
+    }
+
+    public void updateTicket(Ticket ticket) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Ticket originalTicket = session.get(Ticket.class, ticket.getId());
+        originalTicket.setCreatedAt(ticket.getCreatedAt());
+        session.merge(originalTicket);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public void deleteTicket(Ticket ticket) {
+        if (ticket != null) {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.remove(ticket);
+            transaction.commit();
+            System.out.println("Ticket deleted");
+            session.close();
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,13 @@ package org.example;
 
 import org.example.CrudServices.ClientCrudService;
 import org.example.CrudServices.PlanetCrudService;
+import org.example.CrudServices.TicketCrudService;
 import org.example.entity.Client;
 import org.example.entity.Planet;
+import org.example.entity.Ticket;
 import org.example.hibernate.HibernateUtils;
 import org.hibernate.SessionFactory;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class Main {
         SessionFactory sessionFactory = hibernateUtils.getSessionFactory();
         ClientCrudService clientCrudService = new ClientCrudService(sessionFactory);
         PlanetCrudService planetCrudService = new PlanetCrudService(sessionFactory);
+        TicketCrudService ticketCrudService = new TicketCrudService(sessionFactory);
 
 //        // TODO create client
 //         Client client = new Client();
@@ -26,21 +30,39 @@ public class Main {
 //        planet.setName("Pluto");
 //        planetCrudService.createPlanet(planet);
 //
+//        //TODO create ticket
+//        Ticket ticket = new Ticket();
+//        ticket.setCreatedAt(LocalDateTime.now());
+//        Client client = clientCrudService.getClient(9L);
+//        Planet fromPlanet = planetCrudService.getPlanet("VEN");
+//        Planet toPlanet = planetCrudService.getPlanet("JUP");
+//        ticket.setClient(client);
+//        ticket.setFromPlanet(fromPlanet);
+//        ticket.setToPlanet(toPlanet);
+//        ticketCrudService.createTicket(ticket);
+//        System.out.println("Ticket created: " + ticket);
+//
 //        //TODO get client by id
 //        Long clientId = 11L;
 //        Client retrievedClient = clientCrudService.getClient(clientId);
 //        System.out.println("Retrieved Client: " + retrievedClient.getName());
 //
 //        //TODO get planet by id
-//        String planetId = "PLU";
+//        String planetId = "VEN";
 //        Planet retrievedPlanet = planetCrudService.getPlanet(planetId);
 //        System.out.println("Retrieved Planet: " + retrievedPlanet.getName());
+//
+//        //TODO get ticket by id
+//        Ticket retrievedTicket = ticketCrudService.getTicket(3L);
 //
 //        // TODO get all clients
 //        clientCrudService.getAllClients();
 //
 //        //TODO get all planets
 //        planetCrudService.getAllPlanets();
+//
+//        //TODO get all tickets
+//        ticketCrudService.getAllTickets();
 //
 //        // TODO update client
 //        Client client = clientCrudService.getClient(11L);
@@ -54,6 +76,11 @@ public class Main {
 //        planetCrudService.updatePlanet(planet);
 //        System.out.println("Updated planet: " + planet.getName());
 //
+//        //TODO update ticket
+//        Ticket ticket = ticketCrudService.getTicket(2L);
+//        ticket.setCreatedAt(LocalDateTime.now());
+//        ticketCrudService.updateTicket(ticket);
+//
 //        // TODO delete the client
 //        Client client = clientCrudService.getClient(11L);
 //        clientCrudService.deleteClient(client);
@@ -63,7 +90,10 @@ public class Main {
 //        Planet planet = planetCrudService.getPlanet("PLU");
 //        planetCrudService.deletePlanet(planet);
 //        System.out.println("Planet Deleted");
-
+//
+//        //TODO delete the ticket
+//        Ticket ticketForDelete = ticketCrudService.getTicket(6L);
+//        ticketCrudService.deleteTicket(ticketForDelete);
 
         hibernateUtils.closeSessionFactory();
         }
